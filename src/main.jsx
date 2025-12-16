f77fec294d518eaceb4dcf6d10240ae30f45eeb5
Maryam Jamali <mj.jamali.it@gmail.com>
2025-12-16 12:45:55 +0330

    Update package.json and main.jsx for GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import store from "./app/store.jsx";
 
 createRoot(document.getElementById("root")).render(
 
-    <BrowserRouter>
+    <BrowserRouter basename="/store-app">
     <Provider store={store}><App /></Provider>
       
     </BrowserRouter>
